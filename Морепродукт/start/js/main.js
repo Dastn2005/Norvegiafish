24e6206ccaa51778b939e8b40da45186b97ef40a
Dastn2005 <114095672+Dastn2005@users.noreply.github.com>
2022-09-21 22:15:02 +0600

    Add files via upload

---
+++ Морепродукт/start/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.header-slider').slick({
+        dots: true, 
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/str.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/str2.svg" alt=""></button>', 
+        vertical: true
+     
+    });
+});
